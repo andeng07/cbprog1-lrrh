c06786cac91fd7a636f811dbf88ecd51ae8f8f4a
Andrei Sumilang <sumilangandreijohn@gmail.com>
2025-10-25 13:06:49 +0800

    feat: implement helper to get forward coordinate of GameObject

--- src/game_object.c
+++ src/game_object.c
@@ -25,3 +25,29 @@ GameObject *empty_game_object(int x, int y, Status status) {
 
     return obj;
 }
+
+int get_forward_coordinate(GameObject *object, int *outX, int *outY) {
+    if (object->direction == UNDEFINED) return 0;
+
+    int deltaX = 0, deltaY = 0;
+
+    switch (object->direction) {
+        case UP:
+            deltaY = -1;
+            break;
+        case RIGHT:
+            deltaX = 1;
+            break;
+        case DOWN:
+            deltaY = 1;
+            break;
+        case LEFT:
+            deltaX = -1;
+            break;
+    }
+
+    *outX = object->x + deltaX;
+    *outY = object->y + deltaY;
+    
+    return 1;
+}
