62e58f41f61e847d4258e76ba0a7a76902b7a15a
Andrei Sumilang <sumilangandreijohn@gmail.com>
2025-10-27 19:13:52 +0800

    feat: add structs to logically group the state of a game

--- include/game.h
+++ include/game.h
@@ -1,6 +1,30 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include "game_board.h"
+#include "game_object.h"
+
+typedef struct GameSettings {
+    int dev_mode;
+} GameSettings;
+
+typedef struct PlayerActions {
+    int forward;
+    int rotate;
+    int sense;
+} PlayerActions;
+
+typedef struct Game {
+    GameBoard board;
+    GameObject player;
+    
+    int is_alive;
+
+    PlayerActions actions;
+
+    int status; // flags
+} Game;
+
 void run();
 
 #endif // GAME_H
