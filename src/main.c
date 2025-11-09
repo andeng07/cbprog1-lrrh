f4d862cc2a74b8458a388daa6dfa656d666e607c
Andrei Sumilang <sumilangandreijohn@gmail.com>
2025-11-09 13:08:59 +0800

    refactor: remove to do comments

--- src/main.c
+++ src/main.c
@@ -9,31 +9,11 @@ Andrei John Sumilang, 12506346
 ***************************************************************************/
 
 /*
- * TODO:
- *      Ask Ms. Azcaraga:
- *          - Is the bonus ONLY applicable to the algorithm of LRRH?
- *              - If not, will bonus points be given to advanced concepts used?
- *          
- *          - For test scripts, it was stated that there is no need to creat test
- *            scripts for functions that only perform displaying on screen. Does
- *            this apply for function that asks for inputs as well?
- *          
- *          - For void functions, are test required?
- *          
- *          - Is it strict to have the variables declared? Why?
- *              - If so, this goes the same for loops?
- * 
- *          - Where's the criteria / rubrics?
- *          
- *          - Do we strictly need to follow the comment?
- */
-
-/*
  * Description: <Describe what this program does briefly>
  * Programmed by: Andrei John Sumilang XC23
  * Last modified: <date when last revision was made>
  * Version: 1.0-SNAPSHOT
-*/
+ */
 
 #include <stdio.h>
 #include "game.h"
