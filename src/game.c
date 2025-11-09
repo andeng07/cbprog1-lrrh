f976ddff4e45173c958acba1a83fc0397cf66bf0
Andrei Sumilang <sumilangandreijohn@gmail.com>
2025-11-09 13:11:03 +0800

    refactor: add function prototype

--- src/game.c
+++ src/game.c
@@ -6,7 +6,54 @@
 #include "io_util.h"
 #include "renderer.h"
 
-#define GAME_WIDTH 125
+void input_object(GameBoard *board, ObjectType type, Status status);
+void input_multiple_objects(GameBoard *board, ObjectType type, int count, Status status);
+int process_move(Game *game, char move);
+void game_loop(GameBoard *board, GameObject *player);
+
+void run() {
+    // screen title
+    print_title();
+
+    char choice = input_in_set("Press [P] to Play or [Q] to Quit: ", (char[]){'P','p','Q','q'});
+
+    if (choice == 'Q' || choice == 'q') {
+        printf("Goodbye!\n");
+        return;
+    }
+
+    // initialize board
+    int board_size = input_in_range("Enter board size", 8, 15);
+    GameBoard *board = create_game_board(board_size);
+
+    // place player-controlled character
+    GameObject player = {LITTLE_RED_RIDING_HOOD, DOWN, VISIBLE, 0, 0};
+    place_object(board, &player, 0, 0);
+
+    // map configuration
+    int pits_count = input_in_range("Enter number of Pits in the map", 1, board->size);
+    input_multiple_objects(board, PIT, pits_count, HIDDEN);
+
+    int flowers_count = input_in_range("Enter number of Flowers in the map", 1, board->size);
+    input_multiple_objects(board, FLOWER, flowers_count, HIDDEN);
+
+    printf("Enter Wolf location:\n");
+    input_object(board, WOLF, HIDDEN);
+
+    printf("Enter Woodsman location:\n");
+    input_object(board, WOODSMAN, HIDDEN);
+
+    printf("Enter Granny location:\n");
+    input_object(board, GRANNY, HIDDEN);
+
+    printf("\n");
+
+    // start game
+    game_loop(board, &player);
+
+    // free memory once done
+    destroy_game_board(board);
+}
 
 void input_object(GameBoard *board, ObjectType type, Status status) {
     int x, y;
@@ -30,7 +77,7 @@ void input_object(GameBoard *board, ObjectType type, Status status) {
     place_object(board, &obj, x, y);
 }
 
-void place_multiple_objects(GameBoard *board, ObjectType type, int count, Status status) {
+void input_multiple_objects(GameBoard *board, ObjectType type, int count, Status status) {
     for (int i = 0; i < count; i++) {
         printf("Enter %s #%d location:\n", get_type_name(type), i + 1);
         input_object(board, type, status);
@@ -86,7 +133,7 @@ void game_loop(GameBoard *board, GameObject *player) {
     game.status = 0;
 
     while(game.is_alive) {
-        print_board(board, GAME_WIDTH);
+        print_board(board);
 
         printf("=====DASHBOARD=======\n");
         printf("Forward: %d\n", game.actions->forward);
@@ -106,42 +153,4 @@ void game_loop(GameBoard *board, GameObject *player) {
         process_move(&game, move);
         system("cls");
     }
-}
-
-void run() {
-    print_title(GAME_WIDTH);
-
-    char choice = input_in_set("Press [P] to Play or [Q] to Quit: ", (char[]){'P','p','Q','q'});
-
-    if (choice == 'Q' || choice == 'q') {
-        printf("Goodbye!\n");
-        return;
-    }
-
-    int board_size = input_in_range("Enter board size", 8, 15);
-    GameBoard *board = create_game_board(board_size);
-
-    GameObject player = {LITTLE_RED_RIDING_HOOD, DOWN, VISIBLE, 0, 0};
-    place_object(board, &player, 0, 0);
-
-    int pits = input_in_range("Enter number of Pits in the map", 1, 10);
-    place_multiple_objects(board, PIT, pits, HIDDEN);
-
-    int flowers = input_in_range("Enter number of Flowers in the map", 1, 10);
-    place_multiple_objects(board, FLOWER, flowers, HIDDEN);
-
-    printf("Enter Wolf location:\n");
-    input_object(board, WOLF, HIDDEN);
-
-    printf("Enter Woodsman location:\n");
-    input_object(board, WOODSMAN, HIDDEN);
-
-    printf("Enter Granny location:\n");
-    input_object(board, GRANNY, HIDDEN);
-
-    printf("\n");
-
-    game_loop(board, &player);
-
-    destroy_game_board(board);
 }
