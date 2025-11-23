0255fc741fbae34f74173951ee9c3e50271af6ef
Andrei Sumilang <sumilangandreijohn@gmail.com>
2025-11-23 17:29:42 +0800

    feat: add more utility function for game rendering

--- include/renderer.h
+++ include/renderer.h
@@ -3,19 +3,90 @@
 
 #include "game.h"
 
-/* Prints the current state of the game pBoard to the console.
+#define CELL_HEIGHT 3
+#define CELL_WIDTH 3
+
+#define SCREEN_HEIGHT 25
+#define SCREEN_WIDTH 120
+
+#define BOX_HEIGHT 5
+
+#define COLOR_DEFAULT 7
+#define COLOR_PLAYER 12
+#define COLOR_GRANNY 13
+#define COLOR_WOODS 10
+#define COLOR_WOLF 4
+#define COLOR_BAKESHOP 14
+#define COLOR_FLOWER 11
+#define COLOR_ALERT 6
+#define COLOR_PIT 8
+
+#define COLOR_BORDER 8
+
+/* Sets the console foreground color.
+ * Precondition: fg is a valid color code.
+ * @param fg The foreground color.
+ */
+void setColor(int fg);
+
+/* Resets the console text color to the default.
+ * Precondition: none.
+ */
+void resetColor(void);
+
+/* Clears the console screen.
+ * Precondition: none.
+ */
+void clearScreen(void);
+
+/* Prints a dialogue line in the specified color.
+ * Precondition: msg is not NULL.
+ * @param msg The message to print.
+ * @param color The color used to print the message.
+ */
+void printDialogueLine(const char *msg, int color);
+
+/* Prints a single line inside a decorative box.
+ * Precondition: msg is not NULL.
+ * @param msg The message to print.
+ */
+void printBoxedLine(const char *msg);
+
+/* Prints the current state of the game board.
  * Precondition: pBoard is not NULL.
  * @param pBoard Pointer to the GameBoard to render.
- * @param width The console width used for alignment or centering.
  */
 void printBoard(GameBoard *pBoard);
 
-/* Prints the game title or banner centered within the given width.
+/* Prints the game title.
  * Precondition: none.
- * @param width The total width of the display area.
  */
 void printTitle(void);
-void printDialogue (void);
+
+/* Prints all dialogue content managed by the dialogue system.
+ * Precondition: none.
+ */
+void printDialogue(void);
+
+/* Renders the dashboard showing player actions and game state.
+ * Precondition: actions and state are not NULL.
+ * @param actions Pointer to PlayerActions.
+ * @param state Pointer to GameState.
+ */
 void renderDashboard(PlayerActions *actions, GameState *state);
+
+/* Prints collision dialogue based on the object type encountered.
+ * Precondition: state is not NULL.
+ * @param type The object type collided with.
+ * @param state Pointer to GameState.
+ */
+void printCollisionDialogue(ObjectType type, GameState *state);
+
+/* Returns the color associated with the given object type.
+ * Precondition: type is valid.
+ * @param type The object type.
+ * @return The color used to represent the type.
+ */
+int getTypeColor(ObjectType type);
 
 #endif /* RENDERER_H */
--- src/renderer.c
+++ src/renderer.c
@@ -1,42 +1,58 @@
-#include "renderer.h"
 #include <stdio.h>
 #include <windows.h>
+#include <string.h>
+#include <conio.h>
+#include "renderer.h"
+#include "io_util.h"
+#include "game_board.h"
 
-void printBoard(GameBoard *pBoard) {
-    int i, j;
-
-    int size = pBoard->nSize;
-
-    for (i = 0; i < size; i++) {
-        for (j = 0; j < size; j++) {
-            GameObject obj = pBoard->cells[i][j];
-            char landmark = ' ';
-
-            if (obj.status == HIDDEN) {
-                landmark = '*';
-            } else {
-                landmark = getTypeMetadata(obj.type)->cLandmark;
-
-                if (obj.type == LITTLE_RED_RIDING_HOOD) {
-                    switch (obj.direction) {
-                        case UP: landmark = '^'; break;
-                        case DOWN: landmark = 'v'; break;
-                        case LEFT: landmark = '<'; break;
-                        case RIGHT: landmark = '>'; break;
-                        default: landmark = '?'; break;
-                    }
-                }
-            }
-            printf("%c ", landmark);
-        }
-        printf("\n");
-    }
-
+void setColor(int fg) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, fg);
 }
 
-void printTitle(void) {
-    int i;
+void resetColor(void) {
+    setColor(COLOR_DEFAULT);
+}
 
+void clearScreen(void) {
+    system("cls");
+}
+
+void printBoxedLine(const char *msg) {
+    int len = (int)strlen(msg);
+    int padding = (SCREEN_WIDTH - 4 - len) / 2;
+    if (padding < 0) padding = 0;
+
+    printf("|");
+    for (int i = 0; i < padding; i++) putchar(' ');
+    printf("%s", msg);
+    for (int i = 0; i < SCREEN_WIDTH - 4 - padding - len; i++) putchar(' ');
+    printf("|\n");
+}
+
+void printDialogueLine(const char *msg, int color) {
+    setColor(color);
+
+    printf(" ");
+    for (int i = 0; i < SCREEN_WIDTH - 2; i++) putchar('-');
+    printf(" \n");
+
+    printBoxedLine(msg);
+
+    printf(" ");
+    for (int i = 0; i < SCREEN_WIDTH - 2; i++) putchar('-');
+    printf(" \n");
+
+    resetColor();
+    printf("Press any key to continue...");
+    getch();
+
+    printf("\033[F\033[F\033[F");
+}
+
+// ------------------------ Title ------------------------
+void printTitle(void) {
     char *frame[] = {
         "__| |______________________________________________________________________________________________________| |__",
         "__   ______________________________________________________________________________________________________   __",
@@ -55,42 +71,155 @@ void printTitle(void) {
         "__   ______________________________________________________________________________________________________   __",
         "  | |                                                                                                      | |  ",
     };
-
     int lineCount = sizeof(frame) / sizeof(frame[0]);
-
-    for (i = 0; i < lineCount; i++) {
-        printf("%s\n", frame[i]);
+    for (int i = 0; i < lineCount; i++) {
+        printCenter(SCREEN_WIDTH, frame[i]);
+        putchar('\n');
     }
 }
 
-void renderDashboard(PlayerActions *actions, GameState *state) {
-    printf("=====DASHBOARD=======\n");
-    printf("Actions:\n");
-    printf("Forward: %d\n", actions->nForward);
-    printf("Rotation: %d\n", actions->nRotate);
-    printf("Sense: %d\n", actions->nSense);
+void printBoard(GameBoard *pBoard) {
+    int size = pBoard->nSize;
+    int totalWidth = size * 4 + 1;
 
-    printf("\n");
+    for (int row = 0; row < size; row++) {
+        int padding = (SCREEN_WIDTH - totalWidth) / 2;
+        if (padding < 0) padding = 0;
+        for (int i = 0; i < padding; i++) putchar(' ');
+        for (int col = 0; col < size; col++) printf("+---");
+        printf("+\n");
 
-    printf("Game state:\n");
-    if (state->nIsBreadPresent) printf("You have bread!\n");
-    if (state->nIsFlowerPresent) printf("You have flower!\n");
-    if (state->nIsWoodsmanPresent) printf("You are with Woodsman!\n");
+        for (int i = 0; i < padding; i++) putchar(' ');
+        for (int col = 0; col < size; col++) {
+            GameObject obj = pBoard->cells[row][col];
+            char ch = ' ';
+            int color = getTypeColor(obj.type);
+
+            if (obj.status == HIDDEN) {
+                ch = '*';
+                color = getTypeColor(EMPTY);
+            }
+            else {
+                ch = getTypeMetadata(obj.type)->cLandmark;
+
+                if (obj.type == LITTLE_RED_RIDING_HOOD) {
+                    switch (obj.direction) {
+                        case UP: ch = '^'; break;
+                        case DOWN: ch = 'v'; break;
+                        case LEFT: ch = '<'; break;
+                        case RIGHT: ch = '>'; break;
+                        default: ch = '?'; break;
+                    }
+                }
+            }
+
+            printf("| ");
+            setColor(color);
+            putchar(ch);
+            resetColor();
+            printf(" ");
+        }
+        printf("|\n");
+    }
+
+    int padding = (SCREEN_WIDTH - totalWidth) / 2;
+    for (int i = 0; i < padding; i++) putchar(' ');
+    for (int col = 0; col < size; col++) printf("+---");
+    printf("+\n");
 }
 
-void printDialogue(void){
-    
 
-    printf ("Once upon a time, there was a sweet little girl named little red riding hood.\n");
-    Sleep (4000);
-    printf ("She wanted to visit grandma today, but first, her grandma is expecting bread and some flowers.\n");
-    Sleep (5000);
-    printf ("And she must be cautious around here, people say there is a wolf that lurks around the forest.\n");
-    Sleep (5000);
-    printf ("And not to mention the dangerous pits that seem to be bottomless\n");
-    Sleep (3000);
-    printf ("LRRH: I hope I get to grandma safely, it's scary around here");
-    Sleep (4000);
-    printf ("\n\n\n\n");
-    printf ("Little red riding hood enters the forest............\n");
+void renderDashboard(PlayerActions *actions, GameState *state) {
+    printBoxedLine("=+=+=+=+=+=+=+=+=+=+=+= PLAYER DASHBOARD =+=+=+=+=+=+=+=+=+=+=+=");
+
+    char buffer[120];
+    snprintf(buffer, sizeof(buffer), "Forward Moves: %d | Rotations: %d | Sense Actions: %d",
+             actions->nForward, actions->nRotate, actions->nSense);
+    printBoxedLine(buffer);
+
+    printBoxedLine("Inventory / Status:");
+    if (state->nIsBreadPresent) { setColor(COLOR_BAKESHOP); printBoxedLine("Bread: Present"); resetColor(); }
+    if (state->nIsFlowerPresent) { setColor(COLOR_FLOWER); printBoxedLine("Flower: Present"); resetColor(); }
+    if (state->nIsWoodsmanPresent) { setColor(COLOR_WOODS); printBoxedLine("Woodsman: Present"); resetColor(); }
+    putchar('\n');
+}
+
+void printDialogue(void) {
+    printDialogueLine("Once upon a time, in a forest shrouded with mist, there was a sweet little girl named Little Red Riding Hood.", COLOR_ALERT);
+    printDialogueLine("Today she planned to visit her Grandma, bringing freshly baked bread and beautiful flowers.", COLOR_BAKESHOP);
+    printDialogueLine("Beware! The forest is full of peril: sly wolves lurk, and bottomless pits await the unwary.", COLOR_ALERT);
+    printDialogueLine("LRRH whispers: 'I must be careful... I hope I get to Grandma safely!'", COLOR_PLAYER);
+    printDialogueLine("Little Red Riding Hood steps into the forest, her red hood bright against the shadows...", COLOR_WOODS);
+}
+
+void printCollisionDialogue(ObjectType type, GameState *state) {
+    switch (type) {
+        case GRANNY:
+            if (state->nIsWoodsmanPresent && state->nIsBreadPresent && state->nIsFlowerPresent) {
+                printDialogueLine("Grandma greets you warmly! You did it!", COLOR_GRANNY);
+                printDialogueLine("The forest dangers were no match for your courage.", COLOR_GRANNY);
+                printDialogueLine("She hugs you tightly, smelling the fresh bread and flowers.", COLOR_GRANNY);
+                printDialogueLine("The End. What a wonderful day!", COLOR_GRANNY);
+            } else {
+                printDialogueLine("Grandma looks at you sadly...", COLOR_GRANNY);
+                printDialogueLine("You weren't able to gather all the items she expected.", COLOR_GRANNY);
+                printDialogueLine("The forest was too harsh today, and fate wasn't kind.", COLOR_GRANNY);
+                printDialogueLine("The End. Perhaps next time will be better.", COLOR_GRANNY);
+            }
+            break;
+
+        case PIT:
+            printDialogueLine("Oh no! You slipped into a bottomless pit!", COLOR_PIT);
+            printDialogueLine("You scream as darkness swallows you...", COLOR_PIT);
+            printDialogueLine("Game Over!", COLOR_PIT);
+            break;
+
+        case WOLF:
+            if (state->nIsBreadPresent) {
+                printDialogueLine("The cunning wolf pounces and snatches your bread!", COLOR_WOLF);
+                printDialogueLine("LRRH: 'Nooo! My bread! I must find another loaf!'", COLOR_PLAYER);
+            } else {
+                printDialogueLine("The wolf lunges at you with ferocious eyes!", COLOR_WOLF);
+                printDialogueLine("LRRH: 'I have nothing to distract him! I must run!'", COLOR_PLAYER);
+                printDialogueLine("The wolf catches you! Game Over!", COLOR_WOLF);
+            }
+            break;
+
+        case WOODSMAN:
+            printDialogueLine("A friendly Woodsman appears!", COLOR_WOODS);
+            printDialogueLine("He tips his hat: 'I'll guide you safely through these woods.'", COLOR_WOODS);
+            break;
+
+        case BAKESHOP:
+            printDialogueLine("You arrive at the Bakeshop.", COLOR_BAKESHOP);
+            printDialogueLine("The baker hands you a warm loaf: 'Here, this will keep you safe from hunger.'", COLOR_BAKESHOP);
+            break;
+
+        case FLOWER:
+            printDialogueLine("You find a radiant flower among the forest undergrowth.", COLOR_FLOWER);
+            printDialogueLine("LRRH: 'This will make Grandma so happy!'", COLOR_PLAYER);
+            break;
+
+        default: break;
+    }
+    resetColor();
+}
+
+int getTypeColor(ObjectType type) {
+    int color;
+
+    switch (type)
+    {
+        case LITTLE_RED_RIDING_HOOD: color = COLOR_PLAYER; break;
+        case GRANNY: color = COLOR_GRANNY; break;
+        case WOODSMAN: color = COLOR_WOODS; break;
+        case WOLF: color = COLOR_WOLF; break;
+        case BAKESHOP: color = COLOR_BAKESHOP; break;
+        case FLOWER: color = COLOR_FLOWER; break;
+        case PIT: color = COLOR_PIT; break;
+
+        default: color = COLOR_DEFAULT; break;
+    }
+
+    return color;
 }
